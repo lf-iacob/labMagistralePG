05d51258a7dfac3a468aa6af6f7b8167c21e2011
lf-iacob <iacob.laura.f@gmail.com>
2025-11-18 12:17:28 +0100

    Create wf_plot.cpp

---
+++ wf_plot.cpp
@@ -0,0 +1,51 @@
+// --- Waveform representation ---
+
+const int n=1024;
+
+void wf(
+	TString outname, //root file name
+	TString filename //file name for the data
+       ){
+
+  //--- Read input file
+  ifstream file;
+  file.open(filename);
+  
+  //---- File ROOT
+  TFile *f = new TFile(outname, "recreate");
+  
+  //--- TTree
+  double v;
+  TTree* ch = new TTree("ch", "wf");
+  ch->Branch("v", &v, "v/D");
+  while(file.good()){
+    file>>v;
+    ch->Fill();
+  }
+  file.close();
+  //int n = ch -> GetEntries();
+
+  //--- Axis (ticks)
+  double x[n], y[n];
+  for (int i=0; i<n; i++){
+    x[i]=i;
+    ch->GetEntry(i);
+    y[i]=v;
+  }
+
+  //--- Plot
+  TGraph *gr = new TGraph(n, x, y);
+  gr->SetTitle("Waveform - single event;Time [ticks];Amplitude [ADCs]");
+  gr->SetLineColor(kViolet+1);
+  gr->SetLineWidth(3);
+  gr->SetMarkerStyle(8);
+  //gr->SetMarkerSize(0.8);
+  gr->SetMarkerColor(kViolet);
+
+  TCanvas *c = new TCanvas();
+  c->SetGrid();
+  gr->Draw("ALP");
+ 
+
+  return;
+}
