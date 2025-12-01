11e2f40abb4e58d495832bda2d591ef7c9f062f6
lf-iacob <iacob.laura.f@gmail.com>
2025-12-01 19:19:25 +0100

    Update prova_heatmap_multiple.cpp

--- sipm/prova_heatmap_multiple.cpp
+++ sipm/prova_heatmap_multiple.cpp
@@ -4,9 +4,9 @@
 #include <TCanvas.h>
 #include <TH2D.h>
 #include <TStyle.h>
-const int ARRAY_SIZE = 1024;  // dimensione array
-const int N = 100;             // punti per calcolare la baseline
-const int NUM_WFS = 100;       // waveform per file
+const int ARRAY_SIZE = 1024;  
+const int N = 100;
+const int NUM_WFS = 10000;
 
 void analizza(TString file_name1, TString file_name2, TString file_name3, TString file_name4, TString file_name5,
 	      TString output="analysis.root") {
@@ -64,28 +64,25 @@ void analizza(TString file_name1, TString file_name2, TString file_name3, TStrin
     int xBins = ARRAY_SIZE;    // numero di campioni
     double xmin = 0;
     double xmax = ARRAY_SIZE;
+    int yBins = 300;         
+    double ymin = 0;        
+    double ymax = 2400;  
 
-    int yBins = 300;           // numero di bin per l'ampiezza
-    double ymin = 0;        // minimo ampiezza (da adattare ai tuoi dati)
-    double ymax = 2400;         // massimo ampiezza (da adattare ai tuoi dati)
-
-    TH2D* h2 = new TH2D("h2", "Heatmap;Index;Amplitude;Counts", 
+    TH2D* h2 = new TH2D("h2", "Heatmap;Time (ticks);Amplitude [ADC];Counts", 
 			xBins, xmin, xmax, yBins, ymin, ymax);
 
-    // Loop sugli eventi
     for(int i=0; i<nEvents; i++){
       dd->GetEntry(i);
       for(int j=0; j<ARRAY_SIZE; j++){
-        h2->Fill(j, a[j]);  // X = indice del campione, Y = ampiezza
+        h2->Fill(j, a[j]); 
       }
     }
 
-    // ---- Disegno
     gStyle->SetPalette(kSolar);
     TCanvas *chp = new TCanvas("chp","Heatmap");
+    chp->SetLogz();
     h2->Draw("COLZ");
 
-    // ---- Salvataggio
     f->Write();
 
 }
