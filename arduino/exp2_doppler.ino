6e3b5fd8773a3270098e0253cf85a9940b139f5a
lf-iacob <iacob.laura.f@gmail.com>
2026-02-10 21:45:01 +0100

    Refactor sound speed and distance measurement functions
    Refactored functions for sound speed and distance measurement. Updated frequency and added comments for clarity.

--- arduino/exp2_doppler.ino
+++ arduino/exp2_doppler.ino
@@ -1,52 +1,54 @@
-float f = 1400;
-//440
-//987.77
-//1400
+// Tested frequencies (Hz): 440, 987.77, 1400
+
+
+float f = 440;            // frequency originally emitted by the speaker
+
+// setup function
 void setup() {
-  Serial.begin(9600);
-  tone(7, f);
-  pinMode(11, OUTPUT);
-  pinMode(10, INPUT);
-  digitalWrite(11, LOW);
+  Serial.begin(9600);     // connection board-PC
+  // ultrasound sensor connections
+  pinMode(11, OUTPUT);    // trigger digital output pin
+  pinMode(10, INPUT);     // echo digital input pin
+  digitalWrite(11, LOW);  // triggers starts as 0
 }
 
-float prendiVelsuono(float lettura) {
-  float T = lettura * (5./1024.) * 100;
-  float vs = 331.3 + (0.606 * T);
+// function to measure sound speed 
+float soundspeed(float lm35_signal) {
+  float voltage = lm35_signal * (5./1024.);   // LSB = 5/2^10 V/ADC
+  float T =  voltage * 100;                   // temperature conversion factor = 100 °C/V
+  float vs = 331.3 + (0.606 * T);             // sound speed depending on the temperature
   return vs;
 }
 
-float prendiDistanza(float vS) {
+// function to measure the distance with the ultrasound sensor
+float distance(float vs) {
   digitalWrite(11, HIGH);
-  delayMicroseconds(1); //10 
+  delayMicroseconds(10); 
   digitalWrite(11, LOW);
-  long durata = pulseIn(10, HIGH);
-  return (durata*vS*1.e-6) / 2;
+  long time = pulseIn(10, HIGH);
+  return (time * vs * 1.e-6) / 2;
 }
 
 void loop() {
-  float lettura1 = analogRead(A0);
-  float vs1 = prendiVelsuono(lettura1);
-  float d1 = prendiDistanza(vs1);
+  // first distance
+  float signalT1 = analogRead(A0);
+  float vs1 = soundspeed(signalT1);
+  float d1 = distance(vs1);
+  // Δt (0.2s)
   delay(200);
-  float lettura2 = analogRead(A0);
-  float vs2 = prendiVelsuono(lettura2);
-  float d2 = prendiDistanza(vs2);
-  float vo = -(d2 - d1) / 0.2;
-  if(d2<0.06)
-  {
-    noTone(7);
-  }
-  else
-  {
-    tone(7,f);
-  }
+  // second distance
+  float signalT2 = analogRead(A0);
+  float vs2 = soundspeed(signalT2);
+  float d2 = distance(vs2);
+  // => object speed (dynamic cart)
+  float vo = (d1 - d2)/0.2;
 
-  if(d2<0.06){
-    Serial.println("Fine tragitto :)");
+  // Measurement of the doppler effect 
+  if(d2<0.06){                        // once the cart reaches the end of the track
+    Serial.println("FINISH LINE");
     Serial.print("vo:");
     Serial.println(vo);
-    float fd = f*(vs2/(vs2-vo));
+    float fd = f*(vs2/(vs2-vo));      // shifted frequency
     Serial.print("fd:"); 
     Serial.println(fd);
     Serial.print(" ");
