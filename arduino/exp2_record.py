a08b89879821360b3c87325b9cca54d65f41e08b
lf-iacob <iacob.laura.f@gmail.com>
2026-02-10 22:12:56 +0100

    Update exp2_record.py

--- arduino/exp2_record.py
+++ arduino/exp2_record.py
@@ -1,65 +1,66 @@
-''' SPETTRO IN FREQUENZA DI UNA REGISTRAZIONE '''
+
+''' ------- FREQUENCY SPECTRUM OF A RECORDING ------- '''
 import numpy as np
 import matplotlib.pyplot as plt
 import soundcard as sc
 from scipy import fft
 
-#Accesso ai dispositivi del computer
+''' --- Recording process --- '''
+# Access to the computer speakers
 speakers = sc.all_speakers()
 default_speaker = sc.default_speaker()
 mics=sc.all_microphones()
 default_mic=sc.default_microphone()
 
-#Lettura dei file .wav
-samplerate2=44100
-n=int(input('Digitare la durata della registrazione in secondi: '))
-print('Inizio prima regitrazione')
-data2=default_mic.record(samplerate=samplerate2, numframes=samplerate2*n)
-print('Fine prima registrazione')
-m=int(input('Fai partire la seconda registrazione '))
-print('Inizio seconda regitrazione')
-data3=default_mic.record(samplerate=samplerate2, numframes=samplerate2*n)
-print('Fine seconda registrazione')
-#Estrazione dei dati del wav
-y2=data2[:,0]
-y3=data3[:,0]
-num2=len(y2)
-t2=np.linspace(0 , num2/samplerate2, num2)
+# Read file .wav
+samplerate=44100
+n=int(input('Duration of the recordings (s): '))
+print(' --- Object at rest: vo = 0 m/s --- ')
+print('START FIRST RECORDING')
+data1=default_mic.record(samplerate=samplerate, numframes=samplerate*n)
+print('END FIRST RECORDING')
+m=int(input('Write any number to start the second recording ---> '))
+print(' --- Object in motion: vo --- ')
+print('START SECOND RECORDING')
+data2=default_mic.record(samplerate=samplerate, numframes=samplerate*n)
+print('END SECOND RECORDING')
 
-#Realizzazione del grafico: waveform
+# Data extraction
+y1=data1[:,0]
+y2=data2[:,0]
+num=len(y1)
+t=np.linspace(0 , num/samplerate, num)
+
+# Waveform
 plt.subplots(figsize=(11,7))
 plt.plot(t2, y2, color='coral')
-plt.plot(t2, y3, color='coral')
+plt.plot(t2, y3, color='blue')
 plt.title('Waveform', size=30)
-plt.xlabel('Tempo (s)', size=20)
-plt.ylabel('Ampiezza (UA)', size=20)
+plt.xlabel('Time (s)', size=20)
+plt.ylabel('Amplitude (a.u.)', size=20)
 plt.show()
 
-''' ANALISI DI FOURIER '''
-#Trasformata e normalizzazione
+
+''' --- Fourier Analysis --- '''
+# Fourier transform
+c1=fft.fft(y1)
+freq1=fft.fftfreq(len(c1), 1.0/samplerate)
+p1=np.abs(c1)**2  #power
+p1_0=max(p1)      #normalisation to the max value
+p1_norm=p1/p1_0
+
 c2=fft.fft(y2)
-freq2=fft.fftfreq(len(c2), 1.0/samplerate2)
-r2=c2.real #parte reale
-i2=c2.imag #parte immaginaria
-p2=np.abs(c2)**2 #potenza
-
-c3=fft.fft(y3)
-freq3=fft.fftfreq(len(c3), 1.0/samplerate2)
-r3=c3.real #parte reale
-i3=c3.imag #parte immaginaria
-p3=np.abs(c3)**2 #potenza
-
-p2_0=max(p2) #normalizzazione
+freq2=fft.fftfreq(len(c2), 1.0/samplerate)
+p2=np.abs(c2)**2
+p2_0=max(p2)
 p2_norm=p2/p2_0
-p3_0=max(p3) #normalizzazione
-p3_norm=p3/p3_0
 
+# Power spectrum
 plt.figure()
-plt.title('Prova strumento: Spettro di potenza normalizzato', fontsize=25)
-plt.plot(freq2[:len(freq2)//2], p2_norm[:len(p2_norm)//2], color='coral', marker='*')
-plt.plot(freq3[:len(freq3)//2], p3_norm[:len(p3_norm)//2], color='blue', marker='*')
+plt.title('Normalised power spectrum')
+plt.plot(freq1[:len(freq1)//2], p1_norm[:len(p1_norm)//2], color='coral', marker='*')
+plt.plot(freq2[:len(freq2)//2], p2_norm[:len(p2_norm)//2], color='blue', marker='*')
 plt.grid(linestyle=':')
-plt.xlabel('Frequenza (Hz)', fontsize=20)
+plt.xlabel('Frequency (Hz)', fontsize=20)
 plt.ylabel('|C_k|^2 norm', fontsize=20)
-plt.savefig('Spettro_potenza_chitarra.png', bbox_inches='tight')
 plt.show()
