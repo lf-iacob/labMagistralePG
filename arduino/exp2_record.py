cc465babc862af623f93eff5472122e6f7039132
lf-iacob <iacob.laura.f@gmail.com>
2026-02-10 23:26:57 +0100

    Update exp2_record.py

--- arduino/exp2_record.py
+++ arduino/exp2_record.py
@@ -6,21 +6,19 @@ import soundcard as sc
 from scipy import fft
 
 ''' --- Recording process --- '''
-# Access to the computer speakers
-speakers = sc.all_speakers()
-default_speaker = sc.default_speaker()
+# Access to the computer microphones
 mics=sc.all_microphones()
 default_mic=sc.default_microphone()
 
 # Read file .wav
 samplerate=44100
-n=int(input('Duration of the recordings (s): '))
-print(' --- Object at rest: vo = 0 m/s --- ')
+n=int(input('\nDuration of the recordings (s): '))
+print('\n --- Object at rest: vo = 0 m/s --- ')
 print('START FIRST RECORDING')
 data1=default_mic.record(samplerate=samplerate, numframes=samplerate*n)
 print('END FIRST RECORDING')
-m=int(input('Write any number to start the second recording ---> '))
-print(' --- Object in motion: vo --- ')
+m=int(input('\nWrite any number to start the second recording ---> '))
+print('\n --- Object in motion: vo --- ')
 print('START SECOND RECORDING')
 data2=default_mic.record(samplerate=samplerate, numframes=samplerate*n)
 print('END SECOND RECORDING')
@@ -33,8 +31,8 @@ t=np.linspace(0 , num/samplerate, num)
 
 # Waveform
 plt.subplots(figsize=(11,7))
-plt.plot(t2, y2, color='coral')
-plt.plot(t2, y3, color='blue')
+plt.plot(t, y1, color='coral')
+plt.plot(t, y2, color='blue')
 plt.title('Waveform', size=30)
 plt.xlabel('Time (s)', size=20)
 plt.ylabel('Amplitude (a.u.)', size=20)
