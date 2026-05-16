0efead3be5d5a9a1ecb54593a54eb8a62d2941b8
lf-iacob <iacob.laura.f@gmail.com>
2026-05-16 11:39:43 +0200

    Update lifetime.cpp

--- muon/lifetime.cpp
+++ muon/lifetime.cpp
@@ -1,12 +1,22 @@
-// Vita media dei muoni
-
+// Muon lifetime
 using namespace std;
 #include <iostream>
 const int L=8*1024;
 const int N=100;
 
 void lt(TString filename1, TString filename2, TString output="output.root"){
- 
+
+  TString ele_type = filename1;
+  ele_type.ReplaceAll("1.adc", "");
+  cout<<"DATATYPE: Electrons "<<ele_type<<endl<<endl;
+  int par_norm = 100, par_plateau = 10, col_hist = kAzure-5, col_fit = kPink-3;
+  if(ele_type=="down"){
+    par_norm=500;
+    par_plateau=300;
+    col_hist=kSpring-1;
+    col_fit= kViolet+1;
+  }
+    
   TFile *f = new TFile(output, "RECREATE");
   
   int a1[1024*8], a2[1024*8], t1, t2, t, s=1200;
@@ -53,29 +63,49 @@ void lt(TString filename1, TString filename2, TString output="output.root"){
 
   int n = dd->GetEntries();
 
+  
   //time histogram
-  TH1I *ht = new TH1I("ht", "Time Histogram;Time (ns);Entries",300,600,23400); //up: 300, 600, 25000; down: 300, 600, 23400
+  TCanvas *r = new TCanvas();
+ 
+  TH1I *ht = new TH1I("ht", "Time Histogram;Time (ns);Entries",300,600,23400);
+  gStyle->SetOptStat(0);
   for(int i=0; i<n; i++) {
     dd->GetEntry(i);
     ht->Fill(t);
   }
 
   TF1 *ee = new TF1("ee","[0]*exp(-x/[1])+[2]", 600, 23400);
-  gStyle->SetOptFit(0111);
-  ee->SetParameters(500, 2000, 300); //up: 100, 2000, 10; down: 500, 2000, 300
-  double tau, tau_err;
-  ht->SetLineColor(kGreen+2);
+  gStyle->SetOptFit(0);
+  ee->SetParameters(par_norm, 2000, par_plateau);
+  double tau, tau_err, norm, plateau, chi2, gdl;
+  ht->SetLineColor(col_hist);
   //ht->SetLineWidth(2);
-  ht->SetFillColor(kGreen+2);
+  ht->SetFillColor(col_hist);
   ht->SetFillStyle(3005);
-  ee->SetLineColor(kPink-3);
+  ee->SetLineColor(col_fit);
   ee->SetLineWidth(3);
-  ht->Fit(ee); 
+  ht->Fit(ee);
+  
   tau=ee->GetParameter(1);
   tau_err=ee->GetParError(1);
+  norm = ee->GetParameter(0);
+  plateau = ee->GetParameter(2);
+  chi2 = ee->GetChisquare();
+  gdl = ee->GetNDF();
+
+  TLegend *leg = new TLegend(0.4, 0.65, 0.88, 0.88);
+  leg->SetTextSize(0.03);
+  leg->AddEntry(ht, Form("Electrons type: %s", ele_type.Data()), "");
+  leg->AddEntry(ht, Form("Bins = %d ; Range (ns) = (%d, %d)", 300, 600, 23400), "f");
+  leg->AddEntry(ee, Form("Lifetime #tau (#mus) = %.2f #pm %.2f", tau/1000, tau_err/1000), "l");
+  leg->AddEntry(ee, Form("#chi^{2}/NDf = %.2f/%.0f", chi2, gdl), "");
+  leg->AddEntry(ee, Form("Norm = %.2f ; Plateau = %.2f", norm, plateau), "");
+  leg -> Draw();
+  r->Update();
   
   cout<<"Tau = "<<tau<<" +/- "<<tau_err<<endl;
 
+  r->Write();
   ht->Write();
   ee->Write();
   f->Write();
