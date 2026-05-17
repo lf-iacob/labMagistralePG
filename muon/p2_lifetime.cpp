e763ba51fcf63a4890f5b059a7305c4871bdae1a
lf-iacob <iacob.laura.f@gmail.com>
2026-05-17 18:19:04 +0200

    Update and rename lifetime.cpp to p2_lifetime.cpp

--- muon/p2_lifetime.cpp
+++ muon/p2_lifetime.cpp
@@ -4,66 +4,35 @@ using namespace std;
 const int L=8*1024;
 const int N=100;
 
-void lt(TString filename1, TString filename2, TString output="output.root"){
+void lt(TString fileroot, TString output="output.root"){
 
-  TString ele_type = filename1;
-  ele_type.ReplaceAll("1.adc", "");
+  TString ele_type = fileroot;
+  ele_type.ReplaceAll(".root", "");
+  ele_type.ReplaceAll("tree_", "");
   cout<<"DATATYPE: Electrons "<<ele_type<<endl<<endl;
-  int par_norm = 100, par_plateau = 10, col_hist = kAzure-5, col_fit = kPink-3;
+  if(output=="output.root") output="lt_" + ele_type + ".root";
+  int par_plateau = 10, col_hist = kAzure-5, col_fit = kPink-3;
   if(ele_type=="down"){
-    par_norm=500;
     par_plateau=300;
     col_hist=kSpring-1;
     col_fit= kViolet+1;
   }
-    
-  TFile *f = new TFile(output, "RECREATE");
-  
+
+  TFile *file_aperto = TFile::Open(fileroot);
+  TTree *dd = (TTree*)file_aperto->Get("dd");
   int a1[1024*8], a2[1024*8], t1, t2, t, s=1200;
   double b1, b2;
-  TTree *dd = new TTree("dd", "dd");
-  dd->Branch("A1", a1, "a1[8192]/I");
-  dd->Branch("B1", &b1, "b1/D");
-  dd->Branch("T1", &t1, "t1/I");
-  dd->Branch("A2", a2, "a2[8192]/I");
-  dd->Branch("B2", &b2, "b2/D");
-  dd->Branch("T2", &t2, "t2/I");
-  dd->Branch("T", &t, "t/I");
-  
-  ifstream file1, file2;
-  file1.open(filename1);
-  file2.open(filename2);
-  while(file1.good() && file2.good()){
-    b1=0.; b2=0.;
-    for(int i=0;i<L;i++){
-      file1>>a1[i];
-      file2>>a2[i];
-      if(i<N){
-	b1+=double(a1[i])/N;
-        b2+=double(a2[i])/N;
-      }
-    }
-    for(int i=1; i<L; i++){
-      if(((b1-a1[i])>s)){
-	t1=i*4;
-	break;
-      } 
-    }
-     for(int i=1; i<L; i++){
-      if(((b2-a2[i])>s)){
-	t2=i*4;
-	break;
-      } 
-    } 
-    t=abs(t2-t1);
-    dd->Fill();
-  }
-  file1.close();
-  file2.close();
-
+  dd->SetBranchAddress("A1", a1);
+  dd->SetBranchAddress("B1", &b1);
+  dd->SetBranchAddress("T1", &t1);
+  dd->SetBranchAddress("A2", a2);
+  dd->SetBranchAddress("B2", &b2);
+  dd->SetBranchAddress("T2", &t2);
+  dd->SetBranchAddress("T", &t);
   int n = dd->GetEntries();
 
-  
+  TFile *f = new TFile(output, "RECREATE");
+
   //time histogram
   TCanvas *r = new TCanvas();
  
@@ -74,13 +43,12 @@ void lt(TString filename1, TString filename2, TString output="output.root"){
     ht->Fill(t);
   }
   ht->SetLineColor(col_hist);
-  //ht->SetLineWidth(2);
   ht->SetFillColor(col_hist);
   ht->SetFillStyle(3005);
   
   TF1 *ee = new TF1("ee","[0]*exp(-x/[1])+[2]", 600, 23400);
   gStyle->SetOptFit(0);
-  ee->SetParameters(par_norm, 2000, par_plateau);
+  ee->SetParameters(400, 2000, par_plateau);
   double tau, tau_err, norm, plateau, chi2, gdl;
   ee->SetLineColor(col_fit);
   ee->SetLineWidth(3);
@@ -105,8 +73,10 @@ void lt(TString filename1, TString filename2, TString output="output.root"){
   
   cout<<"Tau = "<<tau<<" +/- "<<tau_err<<endl;
 
+  f->cd();
   r->Write();
   ht->Write();
   ee->Write();
   f->Write();
+  file_aperto->Close();
 }
