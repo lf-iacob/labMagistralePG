642296981d5ddbb4894d39fdb98fdf94bfb94e6f
lf-iacob <iacob.laura.f@gmail.com>
2026-05-18 11:58:50 +0200

    Update p3_systematic.cpp

--- muon/p3_systematic.cpp
+++ muon/p3_systematic.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 const int L = 8 * 1024;
 const int N = 100;
-const int P = 15;
+const int P = 10;
+
+void ProgressBar(Long64_t current, Long64_t total) {
+    if (total <= 0) return;
+    int barWidth = 70;
+    float progress = (float)current / total;
+
+    fprintf(stdout, "\r[");
+    int pos = barWidth * progress;
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) fprintf(stdout, "=");
+        else if (i == pos) fprintf(stdout, ">");
+        else fprintf(stdout, " ");
+    }
+    fprintf(stdout, "] %d%% ", int(progress * 100));
+    fflush(stdout);
+}
 
 void sys(TString fileroot, TString output="output.root") {
 
@@ -12,12 +28,12 @@ void sys(TString fileroot, TString output="output.root") {
   ele_type.ReplaceAll("tree_", "");
   cout<<"DATATYPE: Electrons "<<ele_type<<endl<<endl;
   if(output=="output.root") output="sys_" + ele_type + ".root";
-  int par_plateau=10, col_hist=kAzure-5, col_fit=kPink-3, col_3d=kDeepSea;
+  int par_plateau=10, col_hist=kAzure-5, col_3d=kDeepSea, col_mark=kBlue;
   if (ele_type == "down") {
     par_plateau=300;
     col_hist=kSpring-1;
-    col_fit=kViolet+1;
     col_3d=kAvocado;
+    col_mark=kGreen;
   }
 
   TFile *file_aperto = TFile::Open(fileroot);
@@ -32,8 +48,12 @@ void sys(TString fileroot, TString output="output.root") {
   dd->SetBranchAddress("T2", &t2);
   dd->SetBranchAddress("T", &t);
   int n = dd->GetEntries();
-
+  
   TFile *f = new TFile(output, "RECREATE");
+
+  cout << "Riempimento istogrammi in corso..." << endl;
+  Long64_t total_ops = (Long64_t)P * P * n;
+  Long64_t current_op = 0;
 
   //time histograms
   TH1I *ht[P][P];
@@ -44,10 +64,16 @@ void sys(TString fileroot, TString output="output.root") {
       for(int i=0; i<n; i++){
 	dd->GetEntry(i);
 	ht[j][k]->Fill(t);
-	ht[j][k]->SetDirectory(0);
+
+	current_op++;   
+	if (current_op % 500 == 0) ProgressBar(current_op, total_ops);
       }
+      ht[j][k]->SetDirectory(0);
     }
   }
+  ProgressBar(total_ops, total_ops);
+  cout << endl << "Riempimento completato." << endl << endl;
+  cout << "Esecuzione dei Fit..." << endl;
 
   //time fit
   TF1 *ee[P][P];
@@ -58,13 +84,15 @@ void sys(TString fileroot, TString output="output.root") {
       ee[j][k]= new TF1(namef,"[0]*exp(-x/[1])+[2]", 600+50*k, 23400);
       ee[j][k]->SetParameters(400, 2000, par_plateau);
       ht[j][k]->Fit(ee[j][k],"Q0");
-      tau[j][k]=ee[j][k]->GetParameter(1);
-      tau_err[j][k]=ee[j][k]->GetParError(1);
+      if(ee[j][k]->GetParameter(1)>1600){
+	tau[j][k]=ee[j][k]->GetParameter(1);
+	tau_err[j][k]=ee[j][k]->GetParError(1);
+      }
     }
   }
   
   // 3D Graph
-  TCanvas *c2d_1 = new TCanvas();
+  /*TCanvas *c2d_1 = new TCanvas();
   TH2D *h2_tau = new TH2D("h2_tau", "#tau histogram;Binning;Width Range;#tau (ns)", 
 			  P, 300, 3300,  
 			  P, 22050, 22800);
@@ -77,10 +105,13 @@ void sys(TString fileroot, TString output="output.root") {
   c2d_1->cd();
   h2_tau->Draw("COLZ");
   h2_tau->Draw("LEGO2");
+  f->cd();
+  h2_tau->Write();
+  c2d_1->Write();*/
   // *****
   TGraph2D *g2_tau = new TGraph2D();
   g2_tau->SetName("g2_tau");
-  g2_tau->SetTitle("#tau vs Binning e Range;Bins;Range width (ns);#tau (ns)");
+  g2_tau->SetTitle("#tau vs Binning et Range;Bins;Range width (ns);#tau (ns)");
   int nPoint = 0;
   for(int j=0; j<P; j++) {
     for(int k=0; k<P; k++) {
@@ -90,7 +121,7 @@ void sys(TString fileroot, TString output="output.root") {
   }
   TCanvas *c2d_2 = new TCanvas();
   g2_tau->SetMarkerStyle(20);
-  g2_tau->SetMarkerColor(kBlue);
+  g2_tau->SetMarkerColor(col_mark);
   gStyle->SetPalette(col_3d);
   c2d_2->cd();
   g2_tau->Draw("SURF1");
@@ -99,22 +130,26 @@ void sys(TString fileroot, TString output="output.root") {
   
   //total hist
   TCanvas *c = new TCanvas();
-  TH1I *h_tau = new TH1I("htau", "Tau Histogram;Tau (ns);Entries",100,2000,2400);
+  TH1I *h_tau = new TH1I("htau", "Tau Histogram;Tau (ns);Entries",200,1800,2400);
   for(int l=0; l<P; l++) {
     for(int m=0; m<P; m++){
       h_tau->Fill(tau[l][m]);
     }
   }
+  h_tau->SetLineWidth(3);
+  h_tau->SetLineColor(col_hist);
+  h_tau->SetFillColor(col_hist);
+  h_tau->SetFillStyle(3005);
   c->cd();
   h_tau->Draw();
   double err_syst = h_tau->GetStdDev();
+  cout << "------------------------------------------" << endl;
   cout<<"Systematic error (ns) = "<<err_syst<<endl;
+  cout << "------------------------------------------" << endl;
 
   f->cd();
   h_tau->Write();
-  h2_tau->Write();
   g2_tau->Write();
-  c2d_1->Write();
   c2d_2->Write();
   c->Write();
   f->Write();
