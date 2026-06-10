94d5ff03636f1b852fe1577038b8c200761b30f8
lf-iacob <iacob.laura.f@gmail.com>
2026-06-10 12:25:39 +0200

    Update p3_dcr.cpp

--- sipm/p3_dcr.cpp
+++ sipm/p3_dcr.cpp
@@ -20,10 +20,10 @@ void dcr(TString file1, //.root (alberi)
 
   /* --------- VBias (intensity) [V] -> Threshold [ADC]
      54.51 (250) -> 30
-     55.11 (40) -> 30
-     55.81 (70) -> 50
-     56.31 (200, 230, 280) -> 60
-     57.01 (a sentimento) -> 70 
+     55.11 (40) -> 48
+     55.81 (70) -> 60
+     56.31 (200, 230, 280) -> 65
+     57.01 (a sentimento) -> 75
   */        
   cout<<"Threshold: "<<th<<endl<<endl;
 
@@ -77,8 +77,8 @@ void dcr(TString file1, //.root (alberi)
   cout<<"    -----> DCR (Hz) = "<<dcr<<" +- "<<dcr_err<<endl<<endl;
 
 
-  double dcr_array[5]={47463.5, 66517.1, 87527.9, 92837.2, 105568},
-    dcrerr_array[5]={367.361, 439.797, 481.908, 495.836, 525.861},
+  double dcr_array[5]={47463.5, 60144.4, 75709.1, 89238.5, 103455},
+    dcrerr_array[5]={367.361, 391.314, 445.049, 480.41, 506.939},
     vbias_array[5]={54.51, 55.11, 55.81, 56.31, 57.01},
     vbiaserr_array[5]={0.01/sqrt(12), 0.01/sqrt(12), 0.01/sqrt(12), 0.01/sqrt(12), 0.01/sqrt(12)};
 
